fa58b7a2abe749c189df5e464cc5aac7bfba6c84
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-05-19 17:11:15 -0700

    Add an ASR via upload demo

---
+++ Demo/TransformersSharpWebDemo.Web/DemoApiClient.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Components.Forms;
+using static TransformersSharp.Pipelines.ObjectDetectionPipeline;
+
+namespace TransformersSharpWebDemo.Web;
+
+public class DemoApiClient(HttpClient httpClient)
+{
+    public async Task<DetectResponse> GetObjectDetectionAsync(string imageUrl, CancellationToken cancellationToken = default)
+    {
+        List<DetectionResult>? detectedObjects = [];
+        var url = imageUrl;
+        DetectRequest detectRequest = new(url); // Replace with actual URL
+        var response = await httpClient.PostAsJsonAsync("/detect", detectRequest, cancellationToken);
+
+        foreach (var detectionResult in await response.Content.ReadFromJsonAsync<DetectionResult[]>(cancellationToken))
+        {
+            detectedObjects.Add(detectionResult);
+        }
+
+        return new(url, detectedObjects?.ToArray() ?? []);
+    }
+
+    public async Task<string> GetTranscribeAsync(IBrowserFile selectedFile)
+    {
+        var content = new MultipartFormDataContent();
+        var stream = selectedFile.OpenReadStream();
+        var fileContent = new StreamContent(stream);
+        fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("audio/flac");
+        content.Add(fileContent, "file", selectedFile.Name);
+
+        // Adjust the API URL as needed for your environment
+        var response = await httpClient.PostAsync("/transcribe", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            throw new Exception($"Transcription failed: {response.ReasonPhrase}");
+        }
+    }
+}
+
+public record DetectRequest(string Url)
+{
+}
+
+public record DetectResponse(string Url, DetectionResult[] DetectionResults)
+{
+}
